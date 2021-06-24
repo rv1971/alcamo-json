1d0c8e75e74cc218636d021388cc876c60d47269
rv1971 <rv1971@web.de>
2021-06-24 18:11:09 +0200

    Initial commit

---
+++ src/JsonDoc.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace alcamo\json;
+
+use alcamo\exception\SyntaxError;
+
+/**
+ * @brief JSON document
+ */
+class JsonDoc extends JsonNode
+{
+    public static function newFromJsonText(
+        string $jsonText,
+        ?int $depth = null,
+        ?int $flags = null
+    ): self {
+        return new static(
+            json_decode($jsonText, false, $depth ?? 512, $flags ?? 0)
+        );
+    }
+
+    /// @copybrief JsonNode::__construct()
+    public function __construct($data)
+    {
+        parent::__construct($data, null, null);
+    }
+
+    /// Get JSON node identified by JSON pointer
+    public function getNode(string $jsonPtr)
+    {
+        if ($jsonPtr[0] != '/') {
+            /** @throw alcamo:.exception::SyntaxError if $jsonPtr does not
+             *  start with a slash. */
+            throw new SyntaxError($jsonPtr, 0, '; not a valid JSON pointer');
+        }
+
+        $current = $this;
+
+        for (
+            $refToken = strtok($jsonPtr, '/');
+            $refToken !== false;
+            $refToken = strtok('/')
+        ) {
+            if (is_object($current)) {
+                $refToken =
+                    str_replace([ '~1', '~0' ], [ '/', '~' ], $refToken);
+
+                $current = $current->$refToken;
+            } else {
+                $current = $current[$refToken];
+            }
+        }
+
+        return $current;
+    }
+}
---
+++ src/JsonNode.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace alcamo\json;
+
+/**
+ * @brief Object node in a JSON tree
+ */
+class JsonNode
+{
+    private $parent_;  ///< ?self
+    private $key_;     ///< int|string
+    private $jsonPtr_; ///< string
+
+    /**
+     * @brief Construct from object or iterable, creating a public property
+     * for each key
+     */
+    protected function __construct($data, ?self $parent, $key)
+    {
+        $this->parent_ = $parent;
+        $this->key_ = $key;
+
+        if (isset($parent)) {
+            $this->jsonPtr_ = "$parent->jsonPtr_/$key";
+        } else {
+            $this->jsonPtr_ = '';
+        }
+
+        foreach ($data as $subKey => $value) {
+            $this->$subKey = $this->createNode(
+                str_replace([ '~', '/' ], [ '~0', '~1' ], $subKey),
+                $value
+            );
+        }
+    }
+
+    public function __toString()
+    {
+        return $this->toJsonText();
+    }
+
+    /**
+     * @brief Get the closest ancestor that is an object
+     *
+     * @attention This is not the immediate parent if the immediate parent is
+     * an array.
+     */
+    public function getParent(): ?self
+    {
+        return $this->parent_;
+    }
+
+    /// JSON pointer identifying the present node
+    public function getJsonPtr(): string
+    {
+        return $this->jsonPtr_;
+    }
+
+    public function toJsonText(?int $flags = null, ?int $depth = null): string
+    {
+        return json_encode($this, $flags ?? 0, $depth ?? 512);
+    }
+
+    /**
+     * @brief Create a document node
+     * - If $value is a nonempty numerically-indexed array, create an array.
+     * - Else, if $value is an object or iterable, call createNode()
+     *   recursively.
+     * - Else, use $value as-is.
+     *
+     * @sa [How to check if PHP array is associative or sequential?](https://stackoverflow.com/questions/173400/how-to-check-if-php-array-is-associative-or-sequential)
+     */
+    private function createNode($key, $value)
+    {
+        switch (true) {
+            case is_array($value)
+                && (isset($value[0]) || array_key_exists(0, $value))
+                && array_keys($value) === range(0, count($value) - 1):
+                $result = [];
+
+                foreach ($value as $subKey => $subValue) {
+                    $result[] =
+                        $this->createNode("$key/$subKey", $subValue);
+                }
+
+                return $result;
+
+            case is_object($value) || is_iterable($value):
+                return new self($value, $this, $key);
+
+            default:
+                return $value;
+        }
+    }
+}
