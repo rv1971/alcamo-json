d34a72c3ac08285ee20d813ae7abc489e938bd4b
rv1971 <rv1971@web.de>
2021-09-14 12:30:12 +0200

    Move methods to JsonDocTrait

---
+++ src/JsonDocTrait.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace alcamo\json;
+
+use alcamo\exception\SyntaxError;
+
+/**
+ * @brief JSON document trait
+ *
+ * Implemented as a trait so that new document classes can be implemented as
+ * child classes of classes derived from JsonNode that use the present trait.
+ */
+trait JsonDocTrait
+{
+    public static function newFromJsonText(
+        string $jsonText,
+        ?int $depth = null,
+        ?int $flags = null
+    ): self {
+        return new static(
+            json_decode($jsonText, false, $depth ?? 512, $flags ?? 0)
+        );
+    }
+
+    /// Get JSON node identified by JSON pointer
+    public function getNode(string $jsonPtr)
+    {
+        if ($jsonPtr[0] != '/') {
+            /** @throw alcamo:.exception::SyntaxError if $jsonPtr does not
+             *  start with a slash. */
+            throw new SyntaxError($jsonPtr, 0, '; not a valid JSON pointer');
+        }
+
+        $current = $this;
+
+        for (
+            $refToken = strtok($jsonPtr, '/');
+            $refToken !== false;
+            $refToken = strtok('/')
+        ) {
+            if (is_object($current)) {
+                $refToken =
+                    str_replace([ '~1', '~0' ], [ '/', '~' ], $refToken);
+
+                $current = $current->$refToken;
+            } else {
+                $current = $current[$refToken];
+            }
+        }
+
+        return $current;
+    }
+}
