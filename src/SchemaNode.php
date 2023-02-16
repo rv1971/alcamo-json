6222370b87ca44a2d7e19ef21c93af80f6007e16
rv1971 <rv1971@web.de>
2023-02-16 12:12:43 +0100

    Use "#" in SchemaNode::CLASS_MAP

--- src/SchemaNode.php
+++ src/SchemaNode.php
@@ -9,22 +9,22 @@ class SchemaNode extends JsonNode
 {
     public const CLASS_MAP = [
         '$defs'                => SchemaMapNode::class,
-        'additionalProperties' => __CLASS__,
-        'allOf'                => [ '*' => __CLASS__ ],
-        'anyOf'                => [ '*' => __CLASS__ ],
-        'contains'             => __CLASS__,
+        'additionalProperties' => '#',
+        'allOf'                => [ '*' => '#' ],
+        'anyOf'                => [ '*' => '#' ],
+        'contains'             => '#',
         'default'              => JsonNode::class,
         'dependentSchemas'     => SchemaMapNode::class,
-        'else'                 => __CLASS__,
-        'examples'             => [ '*' => __CLASS__ ],
-        'if'                   => __CLASS__,
-        'items'                => __CLASS__,
-        'not'                  => __CLASS__,
-        'oneOf'                => [ '*' => __CLASS__ ],
+        'else'                 => '#',
+        'examples'             => [ '*' => '#' ],
+        'if'                   => '#',
+        'items'                => '#',
+        'not'                  => '#',
+        'oneOf'                => [ '*' => '#' ],
         'patternProperties'    => SchemaMapNode::class,
-        'prefixItems'          => [ '*' => __CLASS__ ],
+        'prefixItems'          => [ '*' => '#' ],
         'properties'           => SchemaMapNode::class,
-        'propertyNames'        => __CLASS__,
-        'then'                 => __CLASS__
+        'propertyNames'        => '#',
+        'then'                 => '#'
     ];
 }
